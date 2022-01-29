9c0766295b2c6b5aa84bb4e489c2b50ff118a28c
alexsandroferrao <alexsandroferrao0203@gmail.com>
2022-01-29 12:00:03 -0300

    Adicionando cypress no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router';
+import Home from './pages/About.page';
+import MoneyPage from './pages/Money.page';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/money" element={<MoneyPage />} />
+        <Route path="*" element={
+          <main style={{ padding: "1rem" }}>
+            <p>Não tem nada nessa página!</p>
+          </main>
+        } />
+      </Routes>
     </div>
   );
 }
